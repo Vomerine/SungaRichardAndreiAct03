0c81296dbae3d34e90d0093b7d8d820eb6673e29
richard <rasunga30@gmail.com>
2022-02-13 14:19:08 +0800

    Implemented add to cart functionality

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,16 @@ app.get('/product/api/products/:id', (req, res) => {
     }
 })
 
+// Hotfix for add to cart
+app.get('/api/products/:id', (req, res) => {
+    const product = data.products.find((x) => x._id === req.params.id);
+    if (product) {
+        res.send(product);
+    }else {
+        res.status(404).send({message: 'Product is not found'})
+    }
+})
+
 app.get('/api/products', (req, res) => {
     res.send(data.products)
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
 
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route, Link} from 'react-router-dom'
 import HomeScreen from "./screens/HomeScreen"
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen';
+import { useSelector } from 'react-redux';
+
 function App() {
+
+  //Adding of badge
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
   return (
     <BrowserRouter>
       <div className = "grid-container">
@@ -13,7 +20,12 @@ function App() {
             </div>
 
             <div>
-                <a href = "/cart">Cart</a>
+                <Link to="/cart">
+                Cart
+                {cartItems.length > 0 && (
+                  <span className="badge">{cartItems.length}</span>
+                )}
+                </Link>
                 <a href = "/signin">Sign In</a>
             </div>
         </header>
